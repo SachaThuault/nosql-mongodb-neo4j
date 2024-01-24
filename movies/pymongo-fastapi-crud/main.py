794e91a33999841f999a7360311156e2863ee0aa
leo <peirolo@yahoo.fr>
2024-01-24 10:26:18 +0100

    neo4j en fastapi

--- movies/pymongo-fastapi-crud/main.py
+++ movies/pymongo-fastapi-crud/main.py
@@ -7,13 +7,34 @@ config = dotenv_values(".env")
 
 app = FastAPI()
 
+
 @app.on_event("startup")
 def startup_db_client():
     app.mongodb_client = MongoClient(config["LOCALHOST_URI"])
     app.database = app.mongodb_client[config["DB_NAME"]]
 
+
 @app.on_event("shutdown")
 def shutdown_db_client():
     app.mongodb_client.close()
 
+
 app.include_router(movie_router, tags=["movies"], prefix="/movies")
+
+
+# from fastapi import FastAPI
+# from pymongo import MongoClient
+# from routes import router as movie_router
+#
+# app = FastAPI()
+#
+# @app.on_event("startup")
+# def startup_db_client():
+#     app.mongodb_client = MongoClient("mongodb+srv://tatdevelops:gjKSGbujMNz7llJs@bddnosql.soetk4a.mongodb.net/?retryWrites=true&w=majority")
+#     app.databaseMongo = app.mongodb_client['bddnosql']
+#
+# @app.on_event("shutdown")
+# def shutdown_db_client():
+#     app.mongodb_client.close()
+#
+# app.include_router(movie_router, tags=["movies"], prefix="/movies")
--- movies/pymongo-fastapi-crud/neo4j.py
+++ movies/pymongo-fastapi-crud/neo4j.py
@@ -1,55 +1,106 @@
+# from urllib.parse import unquote
+# from flask import Flask, jsonify, request
+# from py2neo import Graph
+#
+# app = Flask(__name__)
+#
+# # Remplace les informations ci-dessous par tes propres informations Neo4j
+# neo4j_uri = "bolt://3.95.167.94:7687"
+# username = "neo4j"
+# password = "stators-guys-brooks"
+#
+# # Initialiser la connexion à la base de données Neo4j
+# graph = Graph(neo4j_uri, auth=(username, password))
+#
+# # Exemple de requête Neo4j
+# def run_neo4j_query(cypher_query):
+#     result = graph.run(cypher_query).data()
+#     return result
+#
+# # Exemple de route pour exposer une requête Neo4j via l'API
+# @app.route('/api/exemple', methods=['GET'])
+# def exemple_api():
+#     cypher_query = "MATCH (n) RETURN n LIMIT 5"
+#     result = run_neo4j_query(cypher_query)
+#     return jsonify(result)
+#
+#
+# # list users who rated a movie - the name of the movie is given in parameter ex : "The Da Vinci Code"
+# @app.route('/api/<title>', methods=['GET'])
+# def get_movie_by_title(title):
+#
+#     decoded_title = unquote(title).replace("%20", " ")
+#     print(decoded_title) # pour check le titre après traitement
+#     cypher_query = (
+#         "MATCH (p1)-[r]->(m) WHERE (p1:Person) AND (m:Movie) AND m.title = $decoded_title AND r:REVIEWED RETURN m.title AS `Title`, collect(p1.name) AS `Reviewers`, r.rating AS `Ratings`"
+#     )
+#     result = graph.run(cypher_query, decoded_title=decoded_title).data()
+#     return jsonify(result)
+#
+# # return a user with the number of movies he has rated and the list of rated movies - the name of the user is given in parameter
+# # ex : "James Thompson"
+# @app.route('/api/user/<user>', methods=['GET'])
+# def get_rated_movies_by_user(user):
+#
+#     decoded_user = unquote(user).replace("%20", " ")
+#     print(decoded_user)# pour check le nom du user après traitement
+#     cypher_query = (
+#         "MATCH (p)-[r]->(m) WHERE p.name = $decoded_user AND (p:Person) AND (m:Movie) AND (r:REVIEWED) RETURN p.name AS `Name`, count(m) AS `Number of rated movies`, collect(m.title) AS `Movie(s)`"
+#     )
+#     result = graph.run(cypher_query, decoded_user=decoded_user).data()
+#     return jsonify(result)
+#
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+
+from fastapi import FastAPI, Path
 from urllib.parse import unquote
-from flask import Flask, jsonify, request
 from py2neo import Graph
 
-app = Flask(__name__)
+app = FastAPI()
 
 # Remplace les informations ci-dessous par tes propres informations Neo4j
-neo4j_uri = "bolt://3.219.31.15:7687"
+neo4j_uri = "bolt://3.95.167.94:7687"
 username = "neo4j"
-password = "thimbles-paragraphs-session"
+password = "stators-guys-brooks"
 
 # Initialiser la connexion à la base de données Neo4j
 graph = Graph(neo4j_uri, auth=(username, password))
 
-# Exemple de requête Neo4j
-def run_neo4j_query(cypher_query):
-    result = graph.run(cypher_query).data()
+# Fonction pour exécuter une requête Neo4j
+def run_neo4j_query(cypher_query, **params):
+    result = graph.run(cypher_query, **params).data()
     return result
 
 # Exemple de route pour exposer une requête Neo4j via l'API
-@app.route('/api/exemple', methods=['GET'])
+@app.get('/api/exemple')
 def exemple_api():
     cypher_query = "MATCH (n) RETURN n LIMIT 5"
     result = run_neo4j_query(cypher_query)
-    return jsonify(result)
+    return result
 
-
-# list users who rated a movie - the name of the movie is given in parameter
-@app.route('/api/<title>', methods=['GET'])
-def get_movie_by_title(title):
-
+# list users who rated a movie - the name of the movie is given in parameter ex : "The Da Vinci Code"
+@app.get('/api/{title}')
+def get_movie_by_title(title: str = Path(..., title="Title")):
     decoded_title = unquote(title).replace("%20", " ")
-    print(decoded_title) # pour check le titre après traitement
     cypher_query = (
         "MATCH (p1)-[r]->(m) WHERE (p1:Person) AND (m:Movie) AND m.title = $decoded_title AND r:REVIEWED RETURN m.title AS `Title`, collect(p1.name) AS `Reviewers`, r.rating AS `Ratings`"
     )
-    result = graph.run(cypher_query, decoded_title=decoded_title).data()
-    return jsonify(result)
+    result = run_neo4j_query(cypher_query, decoded_title=decoded_title)
+    return result
 
 # return a user with the number of movies he has rated and the list of rated movies - the name of the user is given in parameter
-@app.route('/api/user/<user>', methods=['GET'])
-def get_rated_movies_by_user(user):
-
+# ex : "James Thompson"
+@app.get('/api/user/{user}')
+def get_rated_movies_by_user(user: str = Path(..., title="User")):
     decoded_user = unquote(user).replace("%20", " ")
-    print(decoded_user)# pour check le nom du user après traitement
     cypher_query = (
         "MATCH (p)-[r]->(m) WHERE p.name = $decoded_user AND (p:Person) AND (m:Movie) AND (r:REVIEWED) RETURN p.name AS `Name`, count(m) AS `Number of rated movies`, collect(m.title) AS `Movie(s)`"
     )
-    result = graph.run(cypher_query, decoded_user=decoded_user).data()
-    return jsonify(result)
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    result = run_neo4j_query(cypher_query, decoded_user=decoded_user)
+    return result
