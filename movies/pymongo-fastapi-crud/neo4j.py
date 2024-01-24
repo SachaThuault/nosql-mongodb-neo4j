544cb6f0d02cb4ca7b0516eb1206d66890bd075f
leo <peirolo@yahoo.fr>
2024-01-24 10:46:27 +0100

    get titres p/r liste titre

--- movies/pymongo-fastapi-crud/neo4j.py
+++ movies/pymongo-fastapi-crud/neo4j.py
@@ -1,63 +1,3 @@
-# from urllib.parse import unquote
-# from flask import Flask, jsonify, request
-# from py2neo import Graph
-#
-# app = Flask(__name__)
-#
-# # Remplace les informations ci-dessous par tes propres informations Neo4j
-# neo4j_uri = "bolt://3.95.167.94:7687"
-# username = "neo4j"
-# password = "stators-guys-brooks"
-#
-# # Initialiser la connexion à la base de données Neo4j
-# graph = Graph(neo4j_uri, auth=(username, password))
-#
-# # Exemple de requête Neo4j
-# def run_neo4j_query(cypher_query):
-#     result = graph.run(cypher_query).data()
-#     return result
-#
-# # Exemple de route pour exposer une requête Neo4j via l'API
-# @app.route('/api/exemple', methods=['GET'])
-# def exemple_api():
-#     cypher_query = "MATCH (n) RETURN n LIMIT 5"
-#     result = run_neo4j_query(cypher_query)
-#     return jsonify(result)
-#
-#
-# # list users who rated a movie - the name of the movie is given in parameter ex : "The Da Vinci Code"
-# @app.route('/api/<title>', methods=['GET'])
-# def get_movie_by_title(title):
-#
-#     decoded_title = unquote(title).replace("%20", " ")
-#     print(decoded_title) # pour check le titre après traitement
-#     cypher_query = (
-#         "MATCH (p1)-[r]->(m) WHERE (p1:Person) AND (m:Movie) AND m.title = $decoded_title AND r:REVIEWED RETURN m.title AS `Title`, collect(p1.name) AS `Reviewers`, r.rating AS `Ratings`"
-#     )
-#     result = graph.run(cypher_query, decoded_title=decoded_title).data()
-#     return jsonify(result)
-#
-# # return a user with the number of movies he has rated and the list of rated movies - the name of the user is given in parameter
-# # ex : "James Thompson"
-# @app.route('/api/user/<user>', methods=['GET'])
-# def get_rated_movies_by_user(user):
-#
-#     decoded_user = unquote(user).replace("%20", " ")
-#     print(decoded_user)# pour check le nom du user après traitement
-#     cypher_query = (
-#         "MATCH (p)-[r]->(m) WHERE p.name = $decoded_user AND (p:Person) AND (m:Movie) AND (r:REVIEWED) RETURN p.name AS `Name`, count(m) AS `Number of rated movies`, collect(m.title) AS `Movie(s)`"
-#     )
-#     result = graph.run(cypher_query, decoded_user=decoded_user).data()
-#     return jsonify(result)
-#
-#
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-
-
 from fastapi import FastAPI, Path
 from urllib.parse import unquote
 from py2neo import Graph
@@ -104,3 +44,21 @@ def get_rated_movies_by_user(user: str = Path(..., title="User")):
     )
     result = run_neo4j_query(cypher_query, decoded_user=decoded_user)
     return result
+
+# example : list_title = ["The Replacements", "The Da Vinci Code"]
+@app.get('/api/movies_by_titles/{list_title}')
+def get_movies_by_titles(list_title: str = Path(..., title="list_title")):
+    decoded_list_title = unquote(list_title).replace('"', "")
+    decoded1_list_title = unquote(decoded_list_title).replace("%20", " ")
+    decoded2_list_title = unquote(decoded1_list_title).replace("]", "")
+    final_decoded_list_title = unquote(decoded2_list_title).replace("[", "")
+    titles_list = [title.strip() for title in final_decoded_list_title.split(",")]
+
+    cypher_query = (
+        "MATCH (m:Movie) WHERE m.title IN $titles_list RETURN DISTINCT m.title AS `Title`"
+    )
+
+    result = run_neo4j_query(cypher_query, titles_list=titles_list)
+    return result
+
+# MATCH (m)<-[]-() WHERE(m:Movie) AND m.title IN ["The Replacements", "The Da Vinci Code"] RETURN DISTINCT m.title AS `Title`
