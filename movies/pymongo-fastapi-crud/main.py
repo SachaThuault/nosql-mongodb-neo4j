cf3d44a45e55b3f9bf644ef9f56d84b2e5538a5b
leo <peirolo@yahoo.fr>
2024-01-24 13:53:39 +0100

    push final

--- movies/pymongo-fastapi-crud/main.py
+++ movies/pymongo-fastapi-crud/main.py
@@ -8,12 +8,12 @@ app = FastAPI()
 @app.on_event("startup")
 def startup_db_client():
     # 2 lignes qui suivent servent à la connexion à la MongoDB, à adapter avec votre URI et nom de base de données
-    app.mongodb_client = MongoClient("mongodb+srv://tatdevelops:gjKSGbujMNz7llJs@bddnosql.soetk4a.mongodb.net/?retryWrites=true&w=majority")
-    app.databaseMongo = app.mongodb_client['bddnosql']
+    app.mongodb_client = MongoClient("mongodb+srv://BDD_NoSQL_USER:KidoER35AZ@project.ls9qqzu.mongodb.net/?retryWrites=true&w=majority")
+    app.databaseMongo = app.mongodb_client['Project']
     # Adapter la ligne qui suit avec vos paramètres de la bdd Neo4
-    app.databaseNeo4j = Graph("bolt://34.201.28.5:7687", auth=("neo4j", "precision-spans-topic"))
+    app.databaseNeo4j = Graph("bolt://3.95.167.94:7687", auth=("neo4j", "stators-guys-brooks"))
     if(app.databaseNeo4j):
-        print('\n    Connexion avec Neo4j réussie : \n',app.databaseNeo4j)
+        print('\n    Connexion avec Neo4j réussie : \n', app.databaseNeo4j)
 
 @app.on_event("shutdown")
 def shutdown_db_client():
